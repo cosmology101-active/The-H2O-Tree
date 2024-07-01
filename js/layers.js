8ea233190cc95873735255ab78488fde040570fd
cosmology101-active <117773180+cosmology101-active@users.noreply.github.com>
2024-07-01 15:47:30 -0400

    Update layers.js
    yes another test

--- js/layers.js
+++ js/layers.js
@@ -12,7 +12,7 @@ addLayer("p", {
     baseResource: "points", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 0.2, // Prestige currency exponent
+    exponent: 0.5, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
         return mult
@@ -20,6 +20,12 @@ addLayer("p", {
     gainExp() { // Calculate the exponent on main currency from bonuses
         return new Decimal(1)
     },
+    upgrades: {
+	    11: {
+		description: "buy this and you will have bought this",
+		cost: new Decimal(100),
+	    };
+    }
     row: 0, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
         {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
