8106604463b4310f4dddbf54ca930dc568536ed4
cosmology101-active <117773180+cosmology101-active@users.noreply.github.com>
2024-07-02 13:31:33 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -2,7 +2,6 @@ addLayer("p", {
     name: "prestige", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "P", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
-    startData() { return {
     color: "#4BDC13",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
     resource: "prestige points", // Name of prestige currency
