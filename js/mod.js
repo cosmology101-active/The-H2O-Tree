79b75c01de89d8de2d097a026ffc8371d6f21478
bdcl999 <jasonyinusa@gmail.com>
2024-07-02 19:50:13 -0400

    lol

--- js/mod.js
+++ js/mod.js
@@ -45,7 +45,7 @@ function getPointGen() {
 	let gain = new Decimal(1)
 	if (hasUpgrade('e', 11)) gain = gain.times(2)
 	if (hasUpgrade('e', 12)) gain = gain.times(upgradeEffect('e', 12))
-	if (hasUpgrade('e', 13)) gain = gain.times(player.points.times(0.2)+1)
+	if (hasUpgrade('e', 13)) gain = gain.times(upgradeEffect('e', 13))
 	return gain
 }
 
