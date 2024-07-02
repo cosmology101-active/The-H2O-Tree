8aba45decec718ed937240ba703edcee50a317b3
cosmology101-active <117773180+cosmology101-active@users.noreply.github.com>
2024-07-02 13:29:21 -0400

    personalized

--- js/mod.js
+++ js/mod.js
@@ -1,8 +1,8 @@
 let modInfo = {
-	name: "The ??? Tree",
+	name: "The H2O Tree",
 	id: "mymod",
-	author: "nobody",
-	pointsName: "points",
+	author: "cosmology101 and bdcl",
+	pointsName: "vapor",
 	modFiles: ["layers.js", "tree.js"],
 
 	discordName: "",
@@ -13,14 +13,14 @@ let modInfo = {
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.0",
-	name: "Literally nothing",
+	num: "0.1",
+	name: "Figuring things out",
 }
 
 let changelog = `<h1>Changelog:</h1><br>
-	<h3>v0.0</h3><br>
-		- Added things.<br>
-		- Added stuff.`
+	<h3>v0.1</h3><br>
+		- Still testing.<br>
+		- Added three upgrades.`
 
 let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
 
