e1789c3802e2a6b60a61295f70357747cb23e177
bdcl999 <jasonyinusa@gmail.com>
2024-07-03 09:00:05 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -114,6 +114,9 @@ addLayer("w", {
     symbol: "W", // This appears on the layer's node. Default is the id with the first letter capitalized.
     position: 0, // Horizontal position within a row. By default, it uses the layer id and sorts in alphabetical order.
     branches: ["h","o"],
+    effect() {
+        return player[this.layer].points.sqrt()
+    },
     startData() { 
         return {
             unlocked: true,
