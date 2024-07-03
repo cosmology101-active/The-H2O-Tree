b48e03fd5fa7d49e43975aaf8a776f07a775612b
cosmology101-active <117773180+cosmology101-active@users.noreply.github.com>
2024-07-02 21:32:46 -0400

    Added Oxygen Layer

--- js/layers.js
+++ js/layers.js
@@ -1,6 +1,6 @@
-addLayer("e", {
-    name: "element", // This is optional, only used in a few places. If absent, it just uses the layer id.
-    symbol: "E", // This appears on the layer's node. Default is the id with the first letter capitalized.
+addLayer("h", {
+    name: "hydrogen", // This is optional, only used in a few places. If absent, it just uses the layer id.
+    symbol: "H", // This appears on the layer's node. Default is the id with the first letter capitalized.
     position: 0, // Horizontal position within a row. By default, it uses the layer id and sorts in alphabetical order.
     startData() { 
         return {
@@ -68,13 +68,44 @@ addLayer("e", {
                 "upgrades"
             ] // Removed extra {} here.
         },
-        "Oxygen": {
+        "Discovery": {
             content: [] // Removed extra {} here.
         },
     },
     row: 0, // Row the layer is in on the tree (0 is the first row).
     hotkeys: [
-        { key: "e", description: "E: Reset for prestige points", onPress() { if (canReset(this.layer)) doReset(this.layer) } },
+        { key: "h", description: "H: Reset for prestige points", onPress() { if (canReset(this.layer)) doReset(this.layer) } },
+    ],
+    layerShown() { return true }
+})
+
+addLayer("o", {
+    name: "oxygen", // This is optional, only used in a few places. If absent, it just uses the layer id.
+    symbol: "O", // This appears on the layer's node. Default is the id with the first letter capitalized.
+    position: 0, // Horizontal position within a row. By default, it uses the layer id and sorts in alphabetical order.
+    startData() { 
+        return {
+            unlocked: true,
+            points: new Decimal(0),
+        }
+    },
+    color: "#F17F3D",
+    requires: new Decimal(10), // Can be a function that takes requirement increases into account.
+    resource: "oxygen", // Name of prestige currency.
+    baseResource: "Vapor", // Name of resource prestige is based on.
+    baseAmount() { return player.points }, // Get the current amount of baseResource.
+    type: "normal", // 'normal': cost to gain currency depends on amount gained. 'static': cost depends on how much you already have.
+    exponent: 0.5, // Prestige currency exponent.
+    gainMult() { // Calculate the multiplier for main currency from bonuses.
+        let mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses.
+        return new Decimal(1)
+    },
+    row: 0, // Row the layer is in on the tree (0 is the first row).
+    hotkeys: [
+        { key: "o", description: "O: Reset for prestige points", onPress() { if (canReset(this.layer)) doReset(this.layer) } },
     ],
     layerShown() { return true }
 })
