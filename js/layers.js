7ca9323be8d8b8498462ca60d8a328c328aba01f
bdcl999 <jasonyinusa@gmail.com>
2024-07-03 09:09:31 -0400

    Update layers.js
    test

--- js/layers.js
+++ js/layers.js
@@ -117,7 +117,7 @@ addLayer("w", {
     effect() {
         return player[this.layer].points.sqrt()
     },
-    effectDescription: String(player[this.layer].points.sqrt()),
+    effectDescription: String(player.w.points.sqrt()),
     startData() { 
         return {
             unlocked: true,
