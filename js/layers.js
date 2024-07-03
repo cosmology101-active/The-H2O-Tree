2749e5d96592fde7334bbabebb2cdfcaecca74c7
bdcl999 <jasonyinusa@gmail.com>
2024-07-03 08:54:28 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -113,6 +113,7 @@ addLayer("w", {
     name: "water", // This is optional, only used in a few places. If absent, it just uses the layer id.
     symbol: "W", // This appears on the layer's node. Default is the id with the first letter capitalized.
     position: 0, // Horizontal position within a row. By default, it uses the layer id and sorts in alphabetical order.
+    branches: ["h","o"],
     startData() { 
         return {
             unlocked: true,
