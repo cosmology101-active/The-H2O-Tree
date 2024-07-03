cdbc0cc6a8f6129f14b57f83eb48054ed5e178b5
bdcl999 <jasonyinusa@gmail.com>
2024-07-02 21:52:34 -0400

    Update layers.js
    efa

--- js/layers.js
+++ js/layers.js
@@ -123,7 +123,7 @@ addLayer("w", {
     requires: new Decimal(10), // Can be a function that takes requirement increases into account.
     resource: "water", // Name of prestige currency.
     baseResource: "hydrogen", // Name of resource prestige is based on.
-    baseAmount() { return player["hydrogen"].points }, // Get the current amount of baseResource.
+    baseAmount() { return player[1].points }, // Get the current amount of baseResource.
     type: "normal", // 'normal': cost to gain currency depends on amount gained. 'static': cost depends on how much you already have.
     exponent: 0.5, // Prestige currency exponent.
     gainMult() { // Calculate the multiplier for main currency from bonuses.
