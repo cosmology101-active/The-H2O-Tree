68e001a57a188c48e7b0082a839b4f90376c5673
bdcl999 <jasonyinusa@gmail.com>
2024-07-02 21:47:31 -0400

    Update layers.js
    test

--- js/layers.js
+++ js/layers.js
@@ -109,3 +109,33 @@ addLayer("o", {
     ],
     layerShown() { return true }
 })
+addLayer("w", {
+    name: "water", // This is optional, only used in a few places. If absent, it just uses the layer id.
+    symbol: "W", // This appears on the layer's node. Default is the id with the first letter capitalized.
+    position: 0, // Horizontal position within a row. By default, it uses the layer id and sorts in alphabetical order.
+    startData() { 
+        return {
+            unlocked: true,
+            points: new Decimal(0),
+        }
+    },
+    color: "#88BBFF",
+    requires: new Decimal(10), // Can be a function that takes requirement increases into account.
+    resource: "water", // Name of prestige currency.
+    baseResource: "hydrogen", // Name of resource prestige is based on.
+    baseAmount() { return player.points }, // Get the current amount of baseResource.
+    type: "normal", // 'normal': cost to gain currency depends on amount gained. 'static': cost depends on how much you already have.
+    exponent: 0.5, // Prestige currency exponent.
+    gainMult() { // Calculate the multiplier for main currency from bonuses.
+        let mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses.
+        return new Decimal(1)
+    },
+    row: 1, // Row the layer is in on the tree (0 is the first row).
+    hotkeys: [
+        { key: "w", description: "W: Reset for prestige points", onPress() { if (canReset(this.layer)) doReset(this.layer) } },
+    ],
+    layerShown() { return true }
+})
