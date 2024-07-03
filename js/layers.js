a67fabd8712d0b4c5cce0b0ca8595358e7e10456
bdcl999 <jasonyinusa@gmail.com>
2024-07-03 08:51:01 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -122,7 +122,7 @@ addLayer("w", {
     color: "#88BBFF",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account.
     resource: "water", // Name of prestige currency.
-    baseResource: "minimum of hydrogen and oxygen/2", // Name of resource prestige is based on.
+    baseResource: "hydrogen", // Name of resource prestige is based on.
     baseAmount() { if (player.h.points.lt(player.o.points.divide(2)) return player.h.points
                  return player.o.points.divide(2)
     }, // Get the current amount of baseResource.
