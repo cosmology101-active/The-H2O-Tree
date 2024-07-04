4b321e0884edead75082cacf133693b1ab323d53
cosmology101-active <117773180+cosmology101-active@users.noreply.github.com>
2024-07-03 21:21:02 -0400

    added achievements

--- js/layers.js
+++ js/layers.js
@@ -1,3 +1,21 @@
+addLayer("a", {
+    name: "achievements", // This is optional, only used in a few places. If absent, it just uses the layer id.
+    symbol: "A", // This appears on the layer's node. Default is the id with the first letter capitalized.
+    position: "side", // Horizontal position within a row. By default, it uses the layer id and sorts in alphabetical order.
+    startData() { 
+        return {
+            unlocked: true,
+            points: new Decimal(0),
+        }
+    },
+    color: "#EFCC04",
+    resource: "achievements", // Name of prestige currency.
+    type: "none", // 'normal': cost to gain currency depends on amount gained. 'static': cost depends on how much you already have.
+    row: 0, // Row the layer is in on the tree (0 is the first row).
+    layerShown() { 
+        return true
+    }
+})
 addLayer("h", {
     name: "hydrogen", // This is optional, only used in a few places. If absent, it just uses the layer id.
     symbol: "H", // This appears on the layer's node. Default is the id with the first letter capitalized.
