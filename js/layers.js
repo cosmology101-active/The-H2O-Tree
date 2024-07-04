a9f93d373ba1e1d5e1660039c49051327c1ee7d0
cosmology101-active <117773180+cosmology101-active@users.noreply.github.com>
2024-07-03 21:24:08 -0400

    Making achievements node smaller and to the side

--- js/layers.js
+++ js/layers.js
@@ -1,7 +1,7 @@
 addLayer("a", {
     name: "achievements", // This is optional, only used in a few places. If absent, it just uses the layer id.
     symbol: "A", // This appears on the layer's node. Default is the id with the first letter capitalized.
-    position: "side", // Horizontal position within a row. By default, it uses the layer id and sorts in alphabetical order.
+    position: 0, // Horizontal position within a row. By default, it uses the layer id and sorts in alphabetical order.
     startData() { 
         return {
             unlocked: true,
@@ -11,7 +11,7 @@ addLayer("a", {
     color: "#EFCC04",
     resource: "achievements", // Name of prestige currency.
     type: "none", // 'normal': cost to gain currency depends on amount gained. 'static': cost depends on how much you already have.
-    row: 0, // Row the layer is in on the tree (0 is the first row).
+    row: "side", // Row the layer is in on the tree (0 is the first row).
     layerShown() { 
         return true
     }
