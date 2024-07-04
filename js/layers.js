eaa32c543a310daea77fdf53ab5b748c9948eaff
cosmology101-active <117773180+cosmology101-active@users.noreply.github.com>
2024-07-04 15:30:53 -0400

    Added nitrogen

--- js/layers.js
+++ js/layers.js
@@ -339,6 +339,58 @@ addLayer("o", {
         else { return false }
     }
 })
+addLayer("n", {
+    name: "nitrogen", // This is optional, only used in a few places. If absent, it just uses the layer id.
+    symbol: "N", // This appears on the layer's node. Default is the id with the first letter capitalized.
+    position: 0, // Horizontal position within a row. By default, it uses the layer id and sorts in alphabetical order.
+    startData() { 
+        return {
+            unlocked: true,
+            points: new Decimal(0),
+        }
+    },
+    color: "#C7F391",
+    requires: new Decimal(1), // Can be a function that takes requirement increases into account.
+    resource: "nitrogen", // Name of prestige currency.
+    baseResource: "minimum(100O,V)", // Name of resource prestige is based on.
+    baseAmount() { if (player.points.lt(player.o.points.multiply(100))) return player.points
+                 return player.o.points.multiply(100) 
+    }, // Get the current amount of baseResource.
+    type: "normal", // 'normal': cost to gain currency depends on amount gained. 'static': cost depends on how much you already have.
+    exponent: 0.2, // Prestige currency exponent.
+    gainMult() { // Calculate the multiplier for main currency from bonuses.
+        let mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses.
+        return new Decimal(1)
+    },
+    row: 0, // Row the layer is in on the tree (0 is the first row).
+    hotkeys: [
+        { key: "n", description: "N: Reset for nitrogen", onPress() { if (canReset(this.layer)) doReset(this.layer) } },
+    ],
+    upgrades: {
+        11: {
+            title: "Nitro",
+            description: "Multiplies Hydrogen gain by 35",
+            cost: new Decimal(5),
+            effect() {
+                return player.o.points.add(1).pow(0.25)
+            },
+        },
+         12: {
+            title: "Volatility",
+            description: "Raise gain of oxygen and nitrogen ^1.5",
+            cost: new Decimal(15),
+            effect() {
+                return new Decimal(1.5)
+            },
+        },
+    },
+    layerShown() {
+        return true
+    }
+})
 addLayer("c", {
     name: "carbon", // This is optional, only used in a few places. If absent, it just uses the layer id.
     symbol: "C", // This appears on the layer's node. Default is the id with the first letter capitalized.
@@ -410,7 +462,7 @@ addLayer("co", {
     },
     requires: new Decimal(25), // Can be a function that takes requirement increases into account.
     resource: "carbon dioxide", // Name of prestige currency.
-    baseResource: "minimum(o,2c)", // Name of resource prestige is based on.
+    baseResource: "O,2C)", // Name of resource prestige is based on.
     baseAmount() { if (player.o.points.lt(player.c.points.multiply(2))) return player.o.points
                  return player.c.points.multiply(2) 
     },
@@ -498,7 +550,7 @@ addLayer("w", {
     color: "#88BBFF",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account.
     resource: "water", // Name of prestige currency.
-    baseResource: "minimum(h,2o)", // Name of resource prestige is based on.
+    baseResource: "H,2O", // Name of resource prestige is based on.
     baseAmount() { if (player.h.points.lt(player.o.points.multiply(2))) return player.h.points
                  return player.o.points.multiply(2)
     }, // Get the current amount of baseResource.
