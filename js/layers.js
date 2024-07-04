9dbd1b755eb881460549ef8987c267380b7a75eb
bdcl999 <jasonyinusa@gmail.com>
2024-07-04 14:47:28 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -366,7 +366,7 @@ addLayer("c", {
 addLayer("co", {
     name: "carbon dioxide", // This is optional, only used in a few places. If absent, it just uses the layer id.
     symbol: "CO", // This appears on the layer's node. Default is the id with the first letter capitalized.
-    position: 1, // Horizontal position within a row. By default, it uses the layer id and sorts in alphabetical order.
+    position: 2, // Horizontal position within a row. By default, it uses the layer id and sorts in alphabetical order.
     branches: ["c","o"],
     startData() { 
         return {
@@ -412,6 +412,70 @@ addLayer("co", {
     },
     layerShown() { 
         return true
+    }
+})
+addLayer("aaa", {
+    name: "aaaaaaaa", // This is optional, only used in a few places. If absent, it just uses the layer id.
+    symbol: "AAAAA", // This appears on the layer's node. Default is the id with the first letter capitalized.
+    position: 1, // Horizontal position within a row. By default, it uses the layer id and sorts in alphabetical order.
+    startData() { 
+        return {
+            unlocked: false,
+            points: new Decimal(0),
+        }
+    },
+    color: "#F17F3D",
+    requires: new Decimal(10), // Can be a function that takes requirement increases into account.
+    resource: "oxygen", // Name of prestige currency.
+    baseResource: "vapor", // Name of resource prestige is based on.
+    baseAmount() { return player.points }, // Get the current amount of baseResource.
+    type: "normal", // 'normal': cost to gain currency depends on amount gained. 'static': cost depends on how much you already have.
+    exponent: 0.5, // Prestige currency exponent.
+    gainMult() { // Calculate the multiplier for main currency from bonuses.
+        let mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses.
+        return new Decimal(1)
+    },
+    row: 0, // Row the layer is in on the tree (0 is the first row).
+    hotkeys: [
+        { key: "o", description: "O: Reset for oxygen", onPress() { if (canReset(this.layer)) doReset(this.layer) } },
+    ],
+    doReset(resettingLayer){
+        let keep=[];
+        if(hasMilestone("w",2)){
+            keep.push("points")}
+        if(hasMilestone("w",1)){
+            keep.push("upgrades")}
+        if(layers[resettingLayer].row>this.row) layerDataReset(this.layer,keep)
+    },
+    upgrades: {
+        11: {
+            title: "Nuclear Fission",
+            description: "Oxygen boosts hydrogen gain",
+            cost: new Decimal(2),
+            effect() {
+                return player.o.points.add(1).pow(0.25)
+            },
+            effectDisplay() { 
+                return "^" + format(upgradeEffect(this.layer, this.id))
+            },
+        },
+        21: {
+            title: "Unlock Next Layer",
+            description: "Unlocks next layer...what will it be?",
+            cost: new Decimal(3),
+            effect() {
+                return player.points.add(1).pow(0.1)
+            },
+            effectDisplay() { 
+                return format(upgradeEffect(this.layer, this.id)) + "x" 
+            },
+        },
+    },
+    layerShown() { 
+        return false
     }
 })
 addLayer("w", {
