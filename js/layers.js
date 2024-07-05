52275a00cda33f2ca3258f6bdc348d24a250bde1
cosmology101-active <117773180+cosmology101-active@users.noreply.github.com>
2024-07-05 17:11:33 -0400

    bug fix?

--- js/layers.js
+++ js/layers.js
@@ -308,7 +308,7 @@ addLayer("h", {
         22: {
             title: "Vapor Inertia",
             description: "Vapor gain slightly increased and raised to the power of ^1.02",
-            currencyDisplayName: "vapor",
+            currencyInternalName: "vapor",
             canAfford() {
                 return player.points.gte(new Decimal(75))
             }
