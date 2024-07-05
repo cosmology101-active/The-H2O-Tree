72b151fc9687a23f677070349c77efee4b74acc1
cosmology101-active <117773180+cosmology101-active@users.noreply.github.com>
2024-07-04 20:32:49 -0400

    Added ammonia

--- js/layers.js
+++ js/layers.js
@@ -388,6 +388,12 @@ addLayer("n", {
             },
         },
     },
+    doReset(resettingLayer){
+        let keep=[];
+        if(hasMilestone("nh",0)){
+            keep.push("upgrades")}
+        if(layers[resettingLayer].row>this.row) layerDataReset(this.layer,keep)
+    },
     layerShown() {
         return true
     }
@@ -440,6 +446,64 @@ addLayer("c", {
             effectDisplay() { 
                 return "^" + format(upgradeEffect(this.layer, this.id))
             },
+        },
+    },
+    layerShown() { 
+        return true
+    }
+})
+addLayer("nh", {
+    name: "ammonia", // This is optional, only used in a few places. If absent, it just uses the layer id.
+    symbol: "AM", // This appears on the layer's node. Default is the id with the first letter capitalized.
+    position: 0, // Horizontal position within a row. By default, it uses the layer id and sorts in alphabetical order.
+    branches: ["n","h"],
+    startData() { 
+        return {
+            unlocked: true,
+            points: new Decimal(0),
+        }
+    },
+    color: "#8622D5",
+    nodeStyle: {
+        "background-image": "radial-gradient(circle, #E7E995, #9B3EE4)"
+    },
+    requires: new Decimal(125), // Can be a function that takes requirement increases into account.
+    resource: "ammonia", // Name of prestige currency.
+    baseResource: "H,3N", // Name of resource prestige is based on.
+    baseAmount() { if (player.h.points.lt(player.n.points.multiply(3))) return player.h.points
+                 return player.n.points.multiply(3) 
+    },
+    type: "static", // 'normal': cost to gain currency depends on amount gained. 'static': cost depends on how much you already have.
+    exponent: 0.3, // Prestige currency exponent.
+    gainMult() { // Calculate the multiplier for main currency from bonuses.
+        let mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses.
+        return new Decimal(1)
+    },
+    row: 2, // Row the layer is in on the tree (0 is the first row).
+    hotkeys: [
+        { key: "nh", description: "Shift+N: Reset for Ammonia", onPress() { if (canReset(this.layer)) doReset(this.layer) } },
+    ],
+    upgrades: {
+        11: {
+            title: "Greenhouse gas",
+            description: "Carbon Dioxide boosts vapor and oxygen generation",
+            cost: new Decimal(2),
+            effect() {
+                return player.points.times(1.02).add(1).pow(0.2)
+            },
+            effectDisplay() { 
+                return format(upgradeEffect(this.layer, this.id)) + "x"
+            },
+        },
+    },
+    milestones: {
+        0: {
+            requirementDescription: "2 ammonia",
+            effectDescription: "keep nitrogen upgrades on reset",
+            done() { return player.nh.points.gte(2) }
         },
     },
     layerShown() { 
@@ -518,32 +582,10 @@ addLayer("co", {
         return true
     }
 })
-addLayer("aaa", {
-    name: "aaaaaaaa", // This is optional, only used in a few places. If absent, it just uses the layer id.
-    symbol: "AAAAA", // This appears on the layer's node. Default is the id with the first letter capitalized.
-    position: 1, // Horizontal position within a row. By default, it uses the layer id and sorts in alphabetical order.
-    startData() { 
-        return {
-            unlocked: false,
-            points: new Decimal(0),
-        }
-    },
-    color: "#F17F3D",
-    requires: new Decimal(10), // Can be a function that takes requirement increases into account.
-    resource: "aaaaaaaaaaaaaaaa", // Name of prestige currency.
-    baseResource: "vapor", // Name of resource prestige is based on.
-    baseAmount() { return new Decimal(0) }, // Get the current amount of baseResource.
-    type: "normal", // 'normal': cost to gain currency depends on amount gained. 'static': cost depends on how much you already have.
-    exponent: 0.5, // Prestige currency exponent.
-    row: 2, // Row the layer is in on the tree (0 is the first row).
-    layerShown() { 
-        return "ghost"
-    }
-})
 addLayer("w", {
     name: "water", // This is optional, only used in a few places. If absent, it just uses the layer id.
     symbol: "W", // This appears on the layer's node. Default is the id with the first letter capitalized.
-    position: 0, // Horizontal position within a row. By default, it uses the layer id and sorts in alphabetical order.
+    position: 1, // Horizontal position within a row. By default, it uses the layer id and sorts in alphabetical order.
     branches: ["h","o"],
     effect() {
         return player[this.layer].points.sqrt().add(1)
