b7289e4a327cef2ab9932157954bd95c47e64d3c
cosmology101-active <117773180+cosmology101-active@users.noreply.github.com>
2024-07-05 16:36:26 -0400

    Version text update

--- js/mod.js
+++ js/mod.js
@@ -13,8 +13,8 @@ let modInfo = {
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.1",
-	name: "Figuring things out",
+	num: "1.0",
+	name: "In development",
 }
 
 let changelog = `<h1>Changelog:</h1><br>
