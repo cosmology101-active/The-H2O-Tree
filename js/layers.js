c8a621716a26a73d41cb6c0787eaefe0339c496c
bdcl999 <jasonyinusa@gmail.com>
2024-07-06 11:27:39 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -389,7 +389,7 @@ addLayer("h", {
             description: "Using the same process as a supernova this will unlock...",
             cost: new Decimal(550),
             unlocked() {
-                return hasUpgrade("h",31) && player[this.layer].bars[0].progress == 1
+                return hasUpgrade("h",31) && player.h.points.gt(300) && player.o.points.gt(300)
             },
         },
         33: {
