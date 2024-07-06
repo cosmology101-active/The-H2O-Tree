67a21aef0cf805a6786f0f288b745d49d6c5c4ab
cosmology101-active <117773180+cosmology101-active@users.noreply.github.com>
2024-07-06 10:58:04 -0400

    Bug fix run 2

--- js/layers.js
+++ js/layers.js
@@ -389,7 +389,7 @@ addLayer("h", {
             description: "Using the same process as a supernova this will unlock...",
             cost: new Decimal(550),
             unlocked() {
-                return hasAchievement("a", 12)
+                return hasUpgrade("h",31) && barProgress("h","progressBar") == 1
             },
         },
         33: {
