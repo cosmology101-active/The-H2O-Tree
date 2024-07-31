874f2e71ad766f0128f79ff3518ff3ba775e4d4e
cosmology101-active <117773180+cosmology101-active@users.noreply.github.com>
2024-07-31 10:52:12 -0400

    Bruh buyable effect typo

--- js/layers/water.js
+++ js/layers/water.js
@@ -84,7 +84,7 @@ addLayer("w", {
         12: {
             title: "Condensation",
             cost(x) { return new Decimal(1.2).mul(x).pow(1.3).round(); },
-            display() { return "Condense water and generate dew. \nCost: " + String(this.cost()) + "\nCurrently: +" + String(buyableEffect("w", 11)) + " dew / tick"; },
+            display() { return "Condense water and generate dew. \nCost: " + String(this.cost()) + "\nCurrently: +" + String(buyableEffect("w", 12)) + " dew / tick"; },
             canAfford() { return player[this.layer].points.gte(this.cost()); },
             buy() {
                 player[this.layer].points = player[this.layer].points.sub(this.cost());
