34ffa9cea1665fbeeb49cd2c2f81d0e00782749f
kevinlejc21 <44309135+kevinlejc21@users.noreply.github.com>
2024-08-22 14:59:19 -0400

    center side icons part 2

--- js/layers/discovery.js
+++ js/layers/discovery.js
@@ -1,6 +1,6 @@
 addLayer("d", {
     name: "discovery", // This is optional, only used in a few places. If absent, it just uses the layer id.
-    symbol: `<img src="resources/icon/sciencebeaker.png" alt="D" class="icon-img">`, // This appears on the layer's node. Default is the id with the first letter capitalized.
+    symbol: `<img src="resources/icon/sciencebeaker.png" alt="D" class="icon-img-small">`, // This appears on the layer's node. Default is the id with the first letter capitalized.
     position: 1, // Horizontal position within a row. By default, it uses the layer id and sorts in alphabetical order.
     startData() { 
         return {
